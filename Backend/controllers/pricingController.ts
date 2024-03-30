dc90e8e56bdb258c526c435167b248fa90588327
Cong Nguyen <nguyencongkhai1407@gmail.com>
2024-03-29 23:56:58 -0500

    Refactor pricingController.ts

--- Backend/controllers/pricingController.ts
+++ Backend/controllers/pricingController.ts
@@ -1,36 +1,37 @@
 import { Request, Response } from 'express';
 
 export const calculatePrice = (req: Request, res: Response) => {
-    try {
-        // Request parameters
-        const { gallonsRequested, deliveryDate, clientLocation } = req.body || {};
+  // try {
+  // Request parameters
+  const { gallonsRequested, deliveryDate, clientLocation } = req.body
 
-        // Validate input
-        if (!gallonsRequested || !deliveryDate || !clientLocation) {
-            return res.status(400).json({ message: 'Missing required fields' });
-        }
+  // Validate input
+  if (!gallonsRequested || !deliveryDate || !clientLocation) {
+    return res.status(400).json({ message: 'Missing required fields' })
+  }
 
-        if (gallonsRequested < 0) {
-            return res.status(400).json({ message: 'Gallons requested cannot be negative' });
-        }
+  if (gallonsRequested < 0) {
+    return res
+      .status(400)
+      .json({ message: 'Gallons requested cannot be negative' })
+  }
 
-        // Pricing calculation
-        // To be replaced with actual pricing logic
-        const suggestedPrice = 2.5; // Current placeholder
-        const totalPrice = parseFloat(gallonsRequested) * suggestedPrice;
+  // Pricing calculation
+  // To be replaced with actual pricing logic
+  const suggestedPrice = 2.5 // Current placeholder
+  const totalPrice = parseFloat(gallonsRequested) * suggestedPrice
 
-        // Response
-        const pricingResult = {
-            totalPrice,
-            // Additional properties
-        };
-        return res.status(200).json({
-            message: 'Price calculated successfully',
-            pricingResult,
-        });
-    } catch (error) {
-        // For errors when doing pricing calculation
-        console.error('Error calculating price:', error); // Move this line inside the try block
-        return res.status(500).json({ error: 'Internal Server Error' });
-    }
+  // Response
+  const pricingResult = {
+    totalPrice,
+    // Additional properties
+  }
+  return res.status(200).json({
+    message: 'Price calculated successfully',
+    pricingResult,
+  })
+  // } catch (error) {
+  //     // For errors when doing pricing calculation
+  //     throw new Error(`Error calculating price: ${(error as Error).message}`)
+  // }
 };
