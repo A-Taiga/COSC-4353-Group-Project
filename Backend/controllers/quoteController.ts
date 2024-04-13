f05cf57bafdeea5b2de7f0baa354d1571c1ea4d7
earmando29 <earmando29@gmail.com>
2024-04-12 20:22:33 -0500

    feat: impl working with postman

--- Backend/controllers/quoteController.ts
+++ Backend/controllers/quoteController.ts
@@ -1,30 +1,28 @@
 import { Request, Response } from 'express';
 import asyncHandler from '../middlewares/asyncHandler';
 import { db } from '../configs/dbConnection';
-import { insertFuelQuoteSchema } from '../types/type';
-import { insertFuelQuote } from '../services/quote.service';
-import { IFuelQuoteData } from '../models/quote.model';
-
-// Pricing model import
-// import { calculateFinalQuote } from '../utils/pricingCalculator';
+import { FuelQuoteInsertSchema } from '../types/type';
+import { createFuelQuote } from '../services/quote.service';
 
 export const submitFuelQuote = asyncHandler(async (req: Request, res: Response) => {
     try {
-        const validatedData = insertFuelQuoteSchema.parse(req.body);
+        const validatedData = FuelQuoteInsertSchema.parse(req.body);
 
-        // Assuming you have a userId available (e.g., from session or JWT token)
-        // You may need to adjust this based on how you're handling user authentication
-        const userId = '721cd5f6-af9b-46e8-a38e-d1e3c362a711'; 
+        const userId = req.body.user_id || 'default-user-id'; 
 
         const quoteDataWithUser = { ...validatedData, userId };
 
-        const result = await db.table('fuel_quotes').insert(quoteDataWithUser).returning('*').execute();
+        const result = await createFuelQuote(req.body);
 
         res.status(200).json({
             message: 'Fuel quote submitted successfully',
-            fuelQuote: result.rows[0], 
+            fuelQuote: result,
         });
     } catch (error) {
-        res.status(400).json({ message: 'Validation failed'});
+        let errorMessage = "Failed to do something exceptional";
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+        res.status(400).json({ message: 'Validation failed', details: errorMessage});
     }
 });
